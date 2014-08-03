9560bb0b8a9e253e722a12253272884ba8de4a0d
Andre Gruening <libredcc@email.de>
2014-08-03 21:04:58 +0100

    start the seeds of documented: add README files

--- s88/s88_hardware.h
+++ s88/s88_hardware.h
@@ -1,5 +1,5 @@
 /* 
- *  Copyright 2014 André Grüning <libredcc@email.de>
+ * Copyright 2014 André Grüning <libredcc@email.de>
  *
  * This file is part of LibreDCC
  *
@@ -16,39 +16,33 @@
  * You should have received a copy of the GNU General Public License
  * along with LibreDCC.  If not, see <http://www.gnu.org/licenses/>.
  */
-/* 
- *  Copyright 2014 André Grüning <libredcc@email.de>
- *
- * This file is part of LibreDCC
- *
- * LibreDCC is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * LibreDCC is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with LibreDCC.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 #ifndef S88_HARDWARE_H
 #define S88_HARDWARE_H 1
 
 #include<avr/io.h>
 
+//! AVR ports to be used (currently port B)
 #define S88_PORT PORTB
 #define S88_DDR  DDRB
 #define S88_PIN PINB
 
 
+//! Connect this AVR pin (Arduino Uno #X) to the LOAD pin of the S88 bus
 #define S88_LOAD_PIN PB0
-#define S88_RESET_PIN PB1
-#define S88_CLOCK_PIN PB2
-#define S88_TEST_PIN PB5
 
+//! Connect this AVR pin (Arduino Uno #X) to the RESET pin of the S88 bus
+#define S88_RESET_PIN PB1
+
+//! Connect this AVR pin (Arduino Uno #X) to the CLOCK pin of the S88 bus
+#define S88_CLOCK_PIN PB2
+
+#if DEBUG
+//! This is the LED on the Ardino Uno board -- for testing and debugging purposes only
+#define S88_TEST_PIN PB5
+#endif 
+
+//! Connect this AVR pin (Arduino Uno #X) to the LINE pin of the S88 bus
 #define S88_DATA_PIN PB3
 
 #define load_on() S88_PORT |= _BV(S88_LOAD_PIN)
@@ -66,7 +60,9 @@
 #define read_s88_data() (S88_PIN & _BV(S88_DATA_PIN))
 
 
-// Enable overflow interrupt generation, ie start the s88 process
+/*! Start reading the S88 bus. For AVR, this enables overflow
+    interrupt generation and that's it as we set up all other things
+    beforehand. */
 #define start_s88() do {TIMSK0 = _BV(OCIE0A);} while(0)
 
 #endif
