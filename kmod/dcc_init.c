7b4735a79ccfbf05206c715dd09b8731c9f19f9f
Andre Gruening <libredcc@email.de>
2015-01-29 20:37:53 +0000

    minor changes in order of function calls in the init function.

--- kmod/dcc_init.c
+++ kmod/dcc_init.c
@@ -8,22 +8,27 @@
  * Credits go to Peter Jay Salzman, Michael Burian, Ori Pomerantz for their The Linux Kernel Module Programming Guide availble at http://www.tldp.org/LDP/lkmpg/2.6/html/index.html
 */
 
-#define DRIVER_AUTHOR "Andre Gruning <libredcc@web.de>"
-#define DRIVER_DESC   "Send and receive DCC signals on a GPIO"
-
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/fs.h>
-
+#include <linux/gpio.h>
 
 #include "dcc_module.h"
 
 // module parameters:
-int dcc_gpio = DEFAULT_DCC_GPIO;
-module_param(dcc_gpio, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-MODULE_PARM_DESC(dcc_gpio, "GPIO pin used for the [input|output] of the dcc signal.");
+static int gpio = DEFAULT_DCC_GPIO;
+module_param(gpio, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(gpio, "GPIO pin used for the [input|output] of the dcc signal.");
+
+static int major = DEVICE_MAJOR; // major device number
+module_param(major, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(major, "Major device number used for dcc device.");
+
+static int irq; // irq used for pin
+
+
 
 // file operations:
 //static int dcc_open(struct inode *, struct file *);
@@ -31,11 +36,8 @@ MODULE_PARM_DESC(dcc_gpio, "GPIO pin used for the [input|output] of the dcc sign
 //static ssize_t dcc_read(struct file *, char *, size_t, loff_t *);
 //static ssize_t dcc_write(struct file *, const char *, size_t, loff_t *);
 
-#define SUCCESS 0
-#define DEVICE_NAME "dcc"
 
-static int major;   // major device number
-static int opened = 0; // has the device been opened?
+//static int opened = 0; // has the device been opened?
 
 static struct file_operations fops = {
   //	.read = dcc_read,
@@ -45,22 +47,66 @@ static struct file_operations fops = {
 };
 
 
-			
 // the beginning and the end:
 
 int __init dcc_init(void)
 {
+  
+  int ret;
+
 	printk(KERN_INFO "DCC service starting.\n");
-	
-	major = register_chrdev(0, DEVICE_NAME, &fops);
+
+	ret = gpio_request(gpio, "DCC Pin");
+	if(ret < 0) {
+	  printk(KERN_ALERT "Requesting GPIO %d failed with %d.\n", gpio, ret);
+	  return ret;
+	}
+
+	ret = gpio_cansleep(gpio);
+	if(ret) {
+	  printk(KERN_ALERT "Aborting because selected GPIO %d can sleep.\n", gpio);
+	  return ret;
+	}
+
+	ret = gpio_direction_input(gpio);
+	if(ret < 0) {
+	  printk(KERN_ALERT "Setting up GPIO %d as input failed with %d\n", gpio, ret);
+	}
+
+	irq = gpio_to_irq(gpio);
+	if(ret < 0) {
+	  printk(KERN_ALERT "Getting interrupt no for GPIO %d failed with %d\n", gpio, ret);
+	}
+
+	//request_irq(irq);
 
 
 
+
+	// setup hardware
+	// \todo What's the pinctrl subsystem? And does it leave on the raspi? Read Documentation/pinctrl.txt
+
+	/*
+- gpio_request(gpio_num, gpio_label)
+- gpio_direction_output(gpio_num, true);
+- gpio_direction_input(gpio_num);
+- gpio_set_value(gpio_num, value);
+- gpio_to_irq(gpio_num);
+- request_irq(...)
+- etc....
+	*/
+
+
+		// setup chardev 	
+	major = register_chrdev(DEVICE_MAJOR, DEVICE_NAME, &fops);
 
 	if (major < 0) {
-	  printk(KERN_ALERT "Registering device " DEVICE_NAME" failed with %d\n", major);
+	  printk(KERN_ALERT "Registering device " DEVICE_NAME " failed with %d\n", major);
 	  return major;
 	}
+	printk(KERN_INFO "DCC service has major device number %d.\n", major);
+
+
 
 	return 0;
 }
@@ -68,9 +114,12 @@ int __init dcc_init(void)
 void __exit dcc_exit(void)
 {
 
-  int ret = unregister_chrdev(major, DEVICE_NAME);
-  if (ret < 0) 
-    printk(KERN_ALERT "Error in unregister_chrdev: %d\n", ret);
+  //free_irq(irq);
+
+  gpio_free(gpio);
+
+  unregister_chrdev(major, DEVICE_NAME); 
+
   printk(KERN_INFO "DCC service ending.\n");
 }
 
--- kmod/dcc_module.h
+++ kmod/dcc_module.h
@@ -1,1 +1,8 @@
-#define DEFAULT_DCC_GPIO 1
+
+#define DRIVER_AUTHOR "Andre Gruning <libredcc@web.de>"
+#define DRIVER_DESC   "Send and receive DCC signals on a GPIO"
+
+#define DEVICE_MAJOR 0
+#define DEVICE_NAME "dcc"
+
+#define DEFAULT_DCC_GPIO 23
