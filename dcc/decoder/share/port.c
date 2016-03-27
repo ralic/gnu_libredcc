be54866650203e236f6d5484283415f2b1a68b3e
Andre Gruening <libredcc@email.de>
2016-03-27 18:31:56 +0100

    model initialisation of ports in an object-oriented way

--- dcc/decoder/share/port.c
+++ dcc/decoder/share/port.c
@@ -2,45 +2,54 @@
 #include <share/switch.h>
 #include <avr/io.h>
 
+#define     TWOSWITCHES_TWOSIGNALS
+//#define FOUR_BLINK
+#if defined TWOSWITCHES_TWOSIGNALS
 port_t ports[] = {
-  [0] = { activate: pulsed_activate,
-	  tick: pulsed_tick,
-	  init: pulsed_init,
-	  timer: 0,
+  [0] = { init: pulsed_init,
 	  ontime: {5,5},
 	  output: {_BV(2), _BV(1)},
   },
-  [1] = { activate: pulsed_activate,
-	  tick: do_nothing,
-	  init: pulsed_init,
-	  timer: 0,
+  [1] = { init: pulsed_init,
 	  ontime: {10,10},
 	  output: {_BV(4), _BV(3)},
   },
-  [2] = { activate: single_permanent_activate,
-	  tick: single_permanent_tick,
-	  init: single_permanent_init,
-	  // timer: 0,
-	  //ontime: {10,10},
+  [2] = { init: single_permanent_init,
 	  output: {_BV(0)},
   }, 
-  [3] = { activate: single_permanent_activate,
-	  tick: single_permanent_tick,
-	  init: single_permanent_init,
-	  // timer: 0,
-	  ontime: {50},
+  [3] = { init: single_permanent_init,
 	  output: {_BV(5)},
   }, 
 };
 
-// const uint8_t NUM_PORTS = sizeof(ports) / sizeof(ports[0]);
+#elif defined FOUR_BLINK
+port_t ports[] = {
+  [0] = { init: single_blink_init,
+	  ontime: {10},
+	  output: {_BV(0)},
+  },
+  [1] = { init: single_blink_init,
+	  ontime: {20},
+	  output: {_BV(1)},
+  },
+  [2] = { init: single_blink_init,
+          ontime: {30},
+	  output: {_BV(3)},
+  }, 
+  [3] = { init: single_blink_init,
+	  ontime: {50},
+	  output: {_BV(4)},
+  }, 
+};
+#else 
+#error No Port configuration defined!
+#endif
 
+// const uint8_t NUM_PORTS = sizeof(ports) / sizeof(ports[0]);
 
 #ifdef NO_LOCAL_STATICS
 static uint8_t count = 0;
 #endif
-
-
 
 /**
  * This function is called repeatedly when the prog button has been pressed (and released)
--- dcc/decoder/share/switch.h
+++ dcc/decoder/share/switch.h
@@ -4,38 +4,10 @@
 #include <inttypes.h>
 #include <share/port.h>
 
-/// initialise a pair of ports
-void pair_init(port_t * const this_port);
+void pulsed_init(port_t * const this_port);
+void permanent_init(port_t * const this_port);
 
-/// initialise a single port (the one with index 0)
-void single_init(port_t * const this_port);
-
-/// do nothing -- a dummy
-void do_nothing(port_t * const this_port);
-
-
-
-void pulsed_activate(port_t * const this_port, const uint8_t gate);
-void pulsed_tick(port_t * const this_port);
-#define pulsed_init pair_init
-
-void permanent_activate(port_t * const this_port, const uint8_t gate);
-#define permanent_tick do_nothing
-#define permanent_init pair_init
-
-
-void single_permanent_activate(port_t * const this_port, const uint8_t gate);
-#define single_permanent_tick do_nothing
-#define single_permanent_init single_init
-
-void single_blink_activate(port_t * const this_port, const uint8_t gate);
-void single_blink_tick(port_t * const this_port);
-#define single_blink_init single_init
-
-
-
-
-
-
+void single_permanent_init(port_t * const this_port);
+void single_blink_init(port_t * const this_port);
 
 #endif
